0e25729d204b5702a9b12bf578afe77ca243f3b0
Zii <93106356+ElainaBot@users.noreply.github.com>
2022-06-22 15:05:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 //=============『 Utama 』================== //
-global.owner = ['6285822347348'] 
+global.owner = ['62881022864785'] 
 global.mods = [] 
 global.prems = [''] 
 
 //=============『 Info Owner 』============== //
-global.nameowner = 'ArullOfc'
-global.numberowner = '6285822347348'
-global.instagram = 'https://instagram.com/sahrulwara_____'
+global.nameowner = 'Zii'
+global.numberowner = '62881022864785'
+global.instagram = 'https://instagram.com/ziistore-id'
 global.github = 'https://github.com/sahrull11'
-global.dana = '085822347348'
-global.pulsa = '085822347348,083843192208'
-global.gopay = ''
+global.dana = '085320499721'
+global.pulsa = '085320499721,085320499721'
+global.gopay = '085320499721'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'ArullBotzMD'
+global.namebot = 'Zii-ID'
 global.gc = 'https://chat.whatsapp.com/Hs5rHr27xu49VRLgdliMfR'
 global.web = 'https://youtu.be/jOhhY7ef_qM' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = 'Permanen 15.000\n1 bulan 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
@@ -23,10 +23,10 @@ global.fotonya1 = 'https://telegra.ph/file/d6b8427c43c0bf596c1e2.jpg' //ganti ja
 global.fotonya2 = 'https://telegra.ph/file/d6b8427c43c0bf596c1e2.jpg' //ini juga ganti 
 global.lolkey = 'Papah-Chan' //biar mudah ngegantinya semisal apikeynya expired:v
 global.zenzkey = 'BagasPrdn' //ganti jadi apikey lu kalau expired
-global.wm = '❚█══ArullBotz‖MD‖'
+global.wm = '❚█══Zii‖ID‖'
 global.watermark = wm
-global.wm2 = '                     「 ArullBotzMD あ⁩ 」'
-global.wm3 = '⫹⫺ ❚█══ArullBotz‖MD‖'
+global.wm2 = '                     「 Zii-ID あ⁩ 」'
+global.wm3 = '⫹⫺ ❚█══Zii‖ID‖'
 global.htki = '––––––『' 
 global.htka = '』––––––'
 global.media = 'https://telegra.ph/file/d6b8427c43c0bf596c1e2.jpg'
@@ -37,7 +37,7 @@ global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*Stiker sedang dibuat*'
 global.packname = 'Sticker By'
-global.author = '© ArullOfc'
+global.author = '© Zii-ID'
 
 //=============『 Apikey 』================== //
 global.APIs = { // API Prefix
